2c378339a0caca544bc85a174c47b0423d137b6d
Arif Chasan <arifchasan92@gmail.com>
2019-01-23 16:29:32 +0700

    api

--- modules/log/controllers/log.php
+++ modules/log/controllers/log.php
@@ -16,12 +16,39 @@ class log extends Admin_Controller {
     	$this->template->build('index');
     }
 
+    public function request()
+    {
+    	$this->check_login();
+
+    	$this->template->build('request');
+    }
+
+    public function migration()
+    {
+    	$this->check_login();
+
+    	$this->template->build('migration');
+    }
+
     public function get_data($collection=null, $offset=0)
     {
     	$trx_code = $this->input->post('trx_code');
 
     	if(!empty($trx_code))
     		$log_trx  = $this->mongo_db->where(array('reference'=>$trx_code))->order_by(array('created_on'=>'DESC'))->get($collection);
+    	else
+    		$log_trx  = $this->mongo_db->order_by(array('created_on'=>'DESC'))->limit(20)->offset($offset)->get($collection);
+
+    	$this->rest->set_data($log_trx);
+    	$this->rest->render();
+    }
+
+    public function get_user_migration($collection='user_migration', $offset=0)
+    {
+    	$phone = $this->input->post('phone');
+
+    	if(!empty($phone))
+    		$log_trx  = $this->mongo_db->where(array('phone'=>$phone))->order_by(array('created_on'=>'DESC'))->get($collection);
     	else
     		$log_trx  = $this->mongo_db->order_by(array('created_on'=>'DESC'))->limit(20)->offset($offset)->get($collection);
 
