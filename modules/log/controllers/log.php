a983f92b44625c4070525e828cfb30e54a94b12c
Arif Chasan <arifchasan92@gmail.com>
2019-01-23 11:56:00 +0700

    test

--- modules/log/controllers/log.php
+++ modules/log/controllers/log.php
@@ -19,9 +19,9 @@ class log extends Admin_Controller {
     public function get_data($collection=null)
     {
     	if(!empty($json['trx_code']))
-    		$log_trx  = $this->mongo_db->where(array('reference'=>$json['trx_code']))->order_by(array('created_on'=>'DESC'))->limit(20)->get($collection);
+    		$log_trx  = $this->mongo_db->where(array('reference'=>$json['trx_code']))->order_by(array('created_on'=>'DESC'))->get($collection);
     	else
-    		$log_trx  = $this->mongo_db->order_by(array('created_on'=>'DESC'))->get($collection);
+    		$log_trx  = $this->mongo_db->order_by(array('created_on'=>'DESC'))->limit(20)->get($collection);
 
     	$this->rest->set_data($log_trx);
     	$this->rest->render();
