856990d6709590fe55a0f77c60bec58a73bbfc02
Arif Chasan <arifchasan92@gmail.com>
2019-01-29 13:12:59 +0700

    outlet menu and view

---
+++ modules/outlets/controllers/outlets.php
@@ -0,0 +1,111 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class outlets extends Admin_Controller {
+	
+	public function __construct() {
+        parent::__construct();
+        $this->load->model('outlets/outlet_model', 'outlet');
+        $this->check_login();
+    }
+
+    public function index()
+    {
+        $this->template
+            ->set('alert', $this->session->flashdata('alert'))
+    		->build('index');
+    }
+
+    // public function add()
+    // {
+    //     $cities           = $this->city->get_all();
+    //     $province         = $this->province->get_all();
+
+    //     $this->template
+    //         ->set('alert', $this->session->flashdata('alert'))
+    //         ->set('title', 'Add Partner')
+    //         ->set('cities', $cities)
+    //         ->set('province', $province)
+    // 		->build('form');
+    // }
+
+    public function edit($id)
+    {
+        $is_exist = $this->outlet->find($id);
+
+        if($is_exist){
+            $outlet = $is_exist;
+
+            $this->template
+                ->set('alert', $this->session->flashdata('alert'))
+                ->set('title', 'Edit outlet')
+                ->set('data', $outlet)
+                ->build('form');
+        }
+    }
+
+    // public function delete($id)
+    // {
+    //     $delete = $this->partner->delete($id);
+
+    //     redirect(site_url('partners'), 'refresh');
+    // }
+
+    public function save($id)
+    {
+        if($this->input->post())
+        {
+            $data = array(
+                'outlet_number' 	=> $this->input->post('outlet_number'),
+                'outlet_name' 		=> $this->input->post('outlet_name'),
+                'level'     		=> $this->input->post('level')
+            );
+
+            $update = $this->outlet->update($id, $data);
+
+            if($update)
+            {
+                redirect(site_url('outlets'), 'refresh');
+            }
+        }
+
+    }
+
+    public function datatables()
+    {
+        $list = $this->outlet->get_datatables();
+        $data = array();
+        $no   = $_POST['start'];
+
+        foreach ($list as $l) {
+            $no++;
+            $row   = array();
+            $row[] = $no;
+            $row[] = $l->name;
+            $row[] = $l->outlet_number;
+            $row[] = $l->outlet_name;
+            $row[] = $l->level;
+            $row[] = $l->dealer_name;
+
+            $btn   = '<a href="'.site_url('outlets/edit/'.$l->id).'" class="btn btn-success btn-sm">
+                        <i class="fa fa-pencil"></i>
+                      </a> &nbsp;';
+
+            $btn  .= '<a href="javascript:;" class="btn btn-primary btn-sm" title="Transaction Lists">
+                        <i class="fa fa-list-ul"></i>
+                      </a>';
+
+            $row[]  = $btn;
+
+            $data[] = $row;
+        }
+ 
+        $output = array(
+            "draw"              => $_POST['draw'],
+            "recordsTotal"      => $this->outlet->count_all(),
+            "recordsFiltered"   => $this->outlet->count_filtered(),
+            "data"              => $data,
+        );
+        //output to json format
+        echo json_encode($output);
+    }
+}
---
+++ modules/outlets/models/outlet_model.php
@@ -0,0 +1,141 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+class outlet_model extends MY_Model {
+
+	protected $table         	= 'customers';
+    protected $key           	= 'id';
+    protected $date_format   	= 'datetime';
+    protected $set_created   	= true;
+    protected $soft_deletes     = true;
+
+    protected $column_order  = array(null, 'name', 'outlet_number', 'outlet_name', 'level', 'dealer_name'); //set column field database for datatable orderable
+    protected $column_search = array('name', 'outlet_number', 'outlet_name', 'level', 'dealer_name'); //set column field database for datatable searchable 
+    protected $order 		 = array('customers.name' => 'asc'); // default order 
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function _get_datatables_query()
+    {
+        $this->db->from($this->table);
+        
+        $i = 0;
+     
+        foreach ($this->column_search as $item) // loop column 
+        {
+            if($_POST['search']['value']) // if datatable send POST for search
+            {
+                 
+                if($i===0) // first loop
+                {
+                    $this->db->open_bracket(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
+                    $this->db->like_not_and($item, $_POST['search']['value']);
+                }
+                else
+                {
+                    $this->db->or_like($item, $_POST['search']['value']);
+                }
+ 
+                if(count($this->column_search) - 1 == $i) //last loop
+                    $this->db->close_bracket(); //close bracket
+            }
+
+            $i++;
+        }
+
+        $this->db->where($this->table.'.deleted', '0');
+        $this->db->where("(outlet_number IS NOT NULL AND outlet_number <> '')");
+
+        if($this->session->userdata('user')->role == 'dealer' || $this->session->userdata('user')->role == 'dealer_ops') 
+        {
+            $this->db->where($this->table.'.dealer_id', $this->session->userdata('user')->dealer_id);
+        }
+                 
+        if(isset($_POST['order'])) // here order processing
+        {
+            $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+        } 
+        else if(isset($this->order))
+        {
+            $order = $this->order;
+            $this->db->order_by(key($order), $order[key($order)]);
+        }
+    }
+ 
+    public function get_datatables()
+    {
+        $this->_get_datatables_query();
+        if($_POST['length'] != -1)
+        $this->db->limit($_POST['length'], $_POST['start']);
+        $query = $this->db->get();
+        return $query->result();
+    }
+ 
+    public function count_filtered()
+    {
+        $this->_get_datatables_query();
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+ 
+    public function count_all()
+    {
+        $this->db->from($this->table);
+        $this->db->where('deleted', '0');
+
+        $from   = $this->input->get('from');
+        $to     = $this->input->get('to');
+
+        if(!empty($from) && !empty($to))
+        {
+            $this->db->where($this->table.'.created_on >=', $from.' 00:00:01');
+            $this->db->where($this->table.'.created_on <=', $to.' 23:59:59');
+        }
+        
+        if($this->session->userdata('user')->role == 'dealer' || $this->session->userdata('user')->role == 'dealer_ops') 
+        {
+            $this->db->where($this->table.'.dealer_id', $this->session->userdata('user')->dealer_id);
+        }
+
+        return $this->db->count_all_results();
+    }
+
+    public function download()
+    {
+        $this->db->select('name, phone, email, outlet_number, outlet_name, dealer_name, account_status, kyc_status, (SELECT created_on FROM transactions WHERE cus_id = customers.id AND status <> \'inquiry\' ORDER BY id DESC LIMIT 1) as last_transaction, created_on as date_registered', false);
+        $this->db->from($this->table);
+        $this->db->where($this->table.'.deleted', '0');
+
+        $from   = $this->input->get('from');
+        $to     = $this->input->get('to');
+
+        if(!empty($from) && !empty($to))
+        {
+            $this->db->where($this->table.'.created_on >=', $from.' 00:00:01');
+            $this->db->where($this->table.'.created_on <=', $to.' 23:59:59');
+        }
+
+        if($this->session->userdata('user')->role == 'dealer' || $this->session->userdata('user')->role == 'dealer_ops') 
+        {
+            $this->db->where($this->table.'.dealer_id', $this->session->userdata('user')->dealer_id);
+        }
+
+        $result = $this->db->get();
+
+        $this->load->dbutil();
+        $this->load->helper('file');
+        $this->load->helper('download');
+
+        date_default_timezone_set("Asia/Jakarta");
+        $filename  = "export_".date('Y-m-d H:i:s').".csv";
+
+        $delimiter = ",";
+        $newline   = "\r\n";
+        $csv_file  = $this->dbutil->csv_from_result($result, $delimiter, $newline);
+
+        force_download($filename, $csv_file);
+    }
+
+}
