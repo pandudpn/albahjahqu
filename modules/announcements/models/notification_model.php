a7302b64e7b55bf931affd54c4c216f5cbd8bf82
Arif Chasan <arifchasan92@gmail.com>
2019-03-05 12:50:38 +0700

    push notif

---
+++ modules/announcements/models/notification_model.php
@@ -0,0 +1,15 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+class notification_model extends MY_Model {
+
+    protected $table            = 'notifications';
+    protected $key           	= 'id';
+    protected $date_format   	= 'datetime';
+    protected $set_created   	= true;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
