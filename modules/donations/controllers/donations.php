1e585b5875a0feb179ea6128b9e1c24abccdb01b
Pandu Dwi Putra <pandudpn@gmail.com>
2019-10-28 10:30:58 +0700

    fixed 0 value

--- modules/donations/controllers/donations.php
+++ modules/donations/controllers/donations.php
@@ -103,16 +103,14 @@ class donations extends Admin_Controller {
             'description'   => $desc,
             'nominal_type'  => $type,
             'due_date'      => $due_date,
+            'slot_amount'   => $amount,
             'target_amount' => $target,
             'type'          => $status,
+            'unit'          => $satuan
         );
 
-        if(isset($amount)) {
-            $data['slot_amount']    = $amount;
-        }
-
-        if(isset($satuan)) {
-            $data['unit']           = $satuan;
+        if($amount == '') {
+            $data['slot_amount']    = 0;
         }
         
         if(!$id){
