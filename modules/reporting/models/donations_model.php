e0b46a517481a7808241740028c831f89a65c83b
Pandu Dwi Putra Nugroho <pandudpn@gmail.com>
2019-10-29 18:32:23 +0700

    reporting donations

---
+++ modules/reporting/models/donations_model.php
@@ -0,0 +1,234 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+class donations_model extends MY_Model {
+
+    protected $table         = 'customer_mutations';
+    protected $tableCustomer = 'customers';
+    protected $key           = 'id';
+    protected $date_format   = 'datetime';
+    protected $set_created   = true;
+    protected $soft_deletes  = true;
+
+    protected $column_order  = array(null, 'customers.account_holder'); //set column field database for datatable orderable
+    protected $column_search = array('customers.account_holder'); //set column field database for datatable searchable 
+    protected $order         = array('customer_mutations.created_on' => 'desc'); // default order 
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function trx_zakat($apps){
+        $eva    = $this->load->database('eva', TRUE);
+
+        $eva->select('SUM(credit) AS total_credit, account_holder AS name, account_id, '.$this->table.'.created_on', false);
+        $eva->from($this->table);
+        $eva->join($this->tableCustomer, $this->tableCustomer.'.id = '.$this->table.'.account_id', 'left');
+        $eva->where('year('.$this->table.'.created_on) =', 'year(curdate())', false);
+        $eva->where('month('.$this->table.'.created_on) =', 'month(curdate())', false);
+        $eva->where('SUBSTRING(transaction_code, 1, 2) = ', $apps);
+        $eva->where('SUBSTRING(transaction_code, 3, 4) = ', 'dona');
+        // $eva->like($this->tableCustomer.'.account_holder', $apps, 'both');
+        $eva->group_by('YEAR('.$this->table.'.created_on), MONTH('.$this->table.'.created_on), DAY('.$this->table.'.created_on)');
+        $eva->order_by($this->table.'.created_on', 'asc');
+
+        $query  = $eva->get();
+        return $query->result();
+    }
+
+    public function trx_zakat_group($apps){
+        $eva    = $this->load->database('eva', TRUE);
+
+        $eva->select('SUM(credit) AS total_credit, account_holder AS name, account_id, '.$this->table.'.created_on', false);
+        $eva->from($this->table);
+        $eva->join($this->tableCustomer, $this->tableCustomer.'.id = '.$this->table.'.account_id', 'left');
+        $eva->where('year('.$this->table.'.created_on) =', 'year(curdate())', false);
+        $eva->where('month('.$this->table.'.created_on) =', 'month(curdate())', false);
+        $eva->like($this->tableCustomer.'.account_holder', $apps, 'both');
+        $eva->group_by('YEAR('.$this->table.'.created_on), MONTH('.$this->table.'.created_on), DAY('.$this->table.'.created_on), '.$this->tableCustomer.'.id');
+        $eva->order_by($this->table.'.created_on', 'asc');
+
+        $query  = $eva->get();
+        return $query->result();
+    }
+
+    public function get_all($apps_name){
+        $eva    = $this->load->database('eva', TRUE);
+
+        $sql    = "SELECT *
+                FROM customers
+                WHERE deleted = 0";
+
+        $sql .= " AND ";
+
+        if(!empty($_POST['search']['value']))
+        {
+            $sql .= "
+                account_holder LIKE '%".$apps_name." ".$eva->escape_like_str($_POST['search']['value'])."%'
+            ";
+        }else{
+            $sql .= " account_holder LIKE '%".$apps_name."%' ";
+        }
+
+        if(isset($_POST['order'])) // here order processing
+        {
+            $sql .= " ORDER BY ".$this->column_order[$_POST['order'][0]['column']]." ".$_POST['order']['0']['dir'];
+        } 
+        else
+        {
+            $sql .= " ORDER BY account_holder ASC";
+        }
+        if($_POST['length'] != -1)
+            $sql .= " LIMIT ".$_POST['start']." ,".$_POST['length'];
+
+        $query  = $eva->query($sql);
+        return $query->result();
+    }
+ 
+    public function get_datatables($alias)
+    {
+        $from   = $this->input->get('from');
+        $to     = $this->input->get('to');
+
+        $eva    = $this->load->database('eva', TRUE);
+        $eva->select('debit, account_holder AS name, account_id, '.$this->table.'.created_on', false);
+        $eva->from($this->table);
+        $eva->join($this->tableCustomer, $this->tableCustomer.'.id = '.$this->table.'.account_id', 'left');
+ 
+        $i = 0;
+     
+        foreach ($this->column_search as $item) // loop column 
+        {
+            if($_POST['search']['value']) // if datatable send POST for search
+            {
+                 
+                if($i===0) // first loop
+                {
+                    $eva->open_bracket(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
+                    $eva->like_not_and($item, $_POST['search']['value']);
+                }
+                else
+                {
+                    $eva->or_like($item, $_POST['search']['value']);
+                }
+ 
+                if(count($this->column_search) - 1 == $i) //last loop
+                    $eva->close_bracket(); //close bracket
+            }
+            $i++;
+        }
+
+        //deleted = 0
+        $eva->where($this->table. '.deleted', '0');
+        $eva->where('SUBSTRING(transaction_code, 1, 4) = ', $alias);
+        $eva->where('SUBSTRING(transaction_code, 5, 4)=','DONA');
+        $eva->where('SUBSTRING(transaction_code, -3)=', 'OUT');
+
+        if(!empty($from) && !empty($to)){
+            $eva->where($this->table.'.created_on >=', $from.' 00:00:01');
+            $eva->where($this->table.'.created_on <=', $to.' 23:59:59');
+        }else{
+            $eva->where('year('.$this->table.'.created_on) =', 'year(curdate())', false);
+            $eva->where('month('.$this->table.'.created_on) =', 'month(curdate())', false);
+        }
+         
+        if(isset($_POST['order'])) // here order processing
+        {
+            $eva->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+        } 
+        else if(isset($this->order))
+        {
+            $order = $this->order;
+            $eva->order_by(key($order), $order[key($order)]);
+        }
+        if($_POST['length'] != -1)
+            $eva->limit($_POST['length'], $_POST['start']);
+        $query = $eva->get();
+        return $query->result();
+    }
+ 
+    public function count_filtered($alias)
+    {
+        $from   = $this->input->get('from');
+        $to     = $this->input->get('to');
+
+        $eva    = $this->load->database('eva', TRUE);
+        $eva->select('credit, account_holder AS name, account_id', false);
+        $eva->from($this->table);
+        $eva->join($this->tableCustomer, $this->tableCustomer.'.id = '.$this->table.'.account_id', 'left');
+ 
+        $i = 0;
+     
+        foreach ($this->column_search as $item) // loop column 
+        {
+            if($_POST['search']['value']) // if datatable send POST for search
+            {
+                 
+                if($i===0) // first loop
+                {
+                    $eva->open_bracket(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
+                    $eva->like_not_and($item, $_POST['search']['value']);
+                }
+                else
+                {
+                    $eva->or_like($item, $_POST['search']['value']);
+                }
+ 
+                if(count($this->column_search) - 1 == $i) //last loop
+                    $eva->close_bracket(); //close bracket
+            }
+            $i++;
+        }
+
+        //deleted = 0
+        $eva->where($this->table. '.deleted', '0');
+        $eva->where('SUBSTRING(transaction_code, 1, 4) = ', $alias);
+        $eva->where('SUBSTRING(transaction_code, 5, 4)=','DONA');
+        $eva->where('SUBSTRING(transaction_code, -3)=', 'OUT');
+
+        if(!empty($from) && !empty($to)){
+            $eva->where($this->table.'.created_on >=', $from.' 00:00:01');
+            $eva->where($this->table.'.created_on <=', $to.' 23:59:59');
+        }else{
+            $eva->where('year('.$this->table.'.created_on) =', 'year(curdate())', false);
+            $eva->where('month('.$this->table.'.created_on) =', 'month(curdate())', false);
+        }
+         
+        if(isset($_POST['order'])) // here order processing
+        {
+            $eva->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+        } 
+        else if(isset($this->order))
+        {
+            $order = $this->order;
+            $eva->order_by(key($order), $order[key($order)]);
+        }
+        $query = $eva->get();
+        return $query->num_rows();
+    }
+ 
+    public function count_all($alias)
+    {
+        $from   = $this->input->get('from');
+        $to     = $this->input->get('to');
+
+        $eva    = $this->load->database('eva', TRUE);
+        $eva->from($this->table);
+        $eva->join($this->tableCustomer, $this->tableCustomer.'.id = '.$this->table.'.account_id');
+        $eva->where($this->table.'.deleted', '0');
+        $eva->where('SUBSTRING(transaction_code, 1, 4) = ', $alias);
+        $eva->where('SUBSTRING(transaction_code, 5, 4)=','DONA');
+        $eva->where('SUBSTRING(transaction_code, -3)=', 'OUT');
+
+        if(!empty($from) && !empty($to)){
+            $eva->where($this->table.'.created_on >=', $from.' 00:00:01');
+            $eva->where($this->table.'.created_on <=', $to.' 23:59:59');
+        }else{
+            $eva->where('year('.$this->table.'.created_on) =', 'year(curdate())', false);
+            $eva->where('month('.$this->table.'.created_on) =', 'month(curdate())', false);
+        }
+
+        return $eva->get()->num_rows();
+    }
+
+}
---
+++ themes/admin/views/modules/reporting/donations.php
@@ -0,0 +1,131 @@
+<div class="row">
+    <div class="col-xl-12">
+        <div class="page-title-box">
+            <h4 class="page-title float-left">Laporan Mutasi Donasi</h4>
+
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
+<!-- end row -->
+
+<div class="row">
+    <div class="col-12">
+        <div class="card-box table-responsive" style="overflow-x: auto; zoom: 0.8;">
+        	<?php if($alert){ ?>
+	    	<div class="alert alert-<?php echo $alert['type']; ?>">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+	    		<?php echo $alert['msg']; ?>
+	    	</div>
+	    	<?php } ?> 
+            <form method="get" id="form">
+                <div class="row" style="margin-bottom: 15px; margin-left: 5px;">
+                    <div class="col-12">Filter : </div>
+                    <div class="col-2"><input type="text" name="from" id="from" class="form-control datepicker" placeholder="Dari tanggal" value="<?php echo $from; ?>"></div>
+                    <div class="col-2"><input type="text" name="to" id="to" class="form-control datepicker" placeholder="Hingga Tanggal" value="<?php echo $to; ?>"></div>
+                    <div class="col-2"><button class="btn btn-primary">Go</button> <a href="<?php echo site_url('reporting/infaq'); ?>" class="btn btn-secondary">Reset</a></div>
+                </div>
+            </form>
+            <table id="datatable" class="table table-striped table-bordered table-responsive">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Nominal</th>
+                        <th>Tanggal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div> <!-- end row -->
+
+<!-- Modal Alert Delete-->
+<div class="modal fade bs-example-modal-sm" id="modal-alert" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="mySmallModalLabel">Alert</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure want to delete this item?</p>
+            </div>
+            <div class="modal-footer">
+                <a id="confirm" href="javascript:;" class="btn btn-danger">Yes, Delete it</a>
+                <a href="javascript:;" class="btn btn-secondary" data-dismiss="modal">Close</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.datepicker').datepicker({
+            format: 'yyyy-mm-dd',
+            autoclose: true
+        });
+
+        Data('','');
+
+        $('#form').submit(function(e){
+            e.preventDefault();
+
+            var from    = $('#from').val();
+            var to      = $('#to').val();
+
+            Data(from, to);
+        });
+
+    });
+
+    function Data(from, to){
+        var url = '<?= site_url("reporting/donations/datatables"); ?>?from='+from+'&to='+to;
+        $('#datatable').DataTable({ 
+            // "scrollX": true,
+            "processing": true, //Feature control the processing indicator.
+            "serverSide": true, //Feature control DataTables' server-side processing mode.
+            "order": [], //Initial no order.
+            "destroy": true,
+
+            // Load data for the table's content from an Ajax source
+            "ajax": {
+                "url": url,
+                "type": "POST"
+            },
+
+            //Set column definition initialisation properties.
+            "columnDefs": [
+                { 
+                    "targets": [0],
+                    "orderable": false,
+                    "data": "no"
+                },
+                {
+                    "targets": [1],
+                    "orderable": false,
+                    "data": "name"
+                },
+                {
+                    "targets": [2],
+                    "orderable": false,
+                    "data": "amount",
+                    "render": function(data, type, row, meta) {
+                        return "Rp " + data
+                    }
+                },
+                {
+                    "targets": [3],
+                    "orderable": false,
+                    "data": "date"
+                }
+            ]
+        });
+    }
+</script>
